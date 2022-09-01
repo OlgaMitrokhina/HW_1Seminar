332d252d594d0257a245c7b975a7f62a713a175d
ольга <mitrohinaolha@ya.ru>
2022-09-01 18:10:16 +0300

    Task 8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -7,12 +7,11 @@ Console.WriteLine("Введите число от 1 до N и программа
 int n;
 Console.WriteLine ("Введите число: ");
 n = Convert.ToInt32(Console.ReadLine());
-while (n < 0)
+while (n <= 0)
 {
     Console.WriteLine("Введите целое положительное число от 1: ");
     n = Convert.ToInt32(Console.ReadLine());
 }
 for (int x = 1; x <= n; x++)
 if (x%2==0)
-Console.Write(" "+ x.ToString());;
-Console.ReadKey();
+Console.Write(x.ToString() + " ");
